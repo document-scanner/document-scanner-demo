f57c3f2e3923ca82ccd3da5f743fdcb29f4cd125
Karl-Philipp Richter <krichter722@aol.de>
2016-12-13 16:13:49 +0100

    added missing storage exceptions to main catch block

--- src/main/java/richtercloud/document/scanner/demo/CommunicationTreePanelDemo.java
+++ src/main/java/richtercloud/document/scanner/demo/CommunicationTreePanelDemo.java
@@ -79,6 +79,8 @@ import richtercloud.reflection.form.builder.jpa.typehandler.ElementCollectionTyp
 import richtercloud.reflection.form.builder.jpa.typehandler.ToManyTypeHandler;
 import richtercloud.reflection.form.builder.jpa.typehandler.ToOneTypeHandler;
 import richtercloud.reflection.form.builder.jpa.typehandler.factory.JPAAmountMoneyMappingTypeHandlerFactory;
+import richtercloud.reflection.form.builder.storage.StorageConfInitializationException;
+import richtercloud.reflection.form.builder.storage.StorageCreationException;
 import richtercloud.reflection.form.builder.storage.StorageException;
 import richtercloud.reflection.form.builder.typehandler.TypeHandler;
 
@@ -102,7 +104,7 @@ public class CommunicationTreePanelDemo extends JFrame {
     private final TagStorage tagStorage = new MemoryTagStorage();
     private final boolean deleteDatabase = true;
 
-    public CommunicationTreePanelDemo() throws IllegalArgumentException, IllegalAccessException, IOException, InstantiationException, InvocationTargetException, NoSuchMethodException, FieldHandlingException, StorageException, SQLException, NoSuchFieldException {
+    public CommunicationTreePanelDemo() throws IllegalArgumentException, IllegalAccessException, IOException, InstantiationException, InvocationTargetException, NoSuchMethodException, FieldHandlingException, StorageException, SQLException, NoSuchFieldException, StorageConfInitializationException, StorageCreationException {
         File databaseDir = new File("/tmp/communication-tree-panel-demo");
         File schemeChecksumFile = new File("/tmp/communcation-tree-panel-demo-checksum-file");
         DerbyEmbeddedPersistenceStorageConf storageConf = new DerbyEmbeddedPersistenceStorageConf(DocumentScanner.ENTITY_CLASSES,
@@ -332,7 +334,7 @@ public class CommunicationTreePanelDemo extends JFrame {
             public void run() {
                 try {
                     new CommunicationTreePanelDemo().setVisible(true);
-                } catch (IllegalArgumentException | IllegalAccessException | IOException | InstantiationException | InvocationTargetException | NoSuchMethodException | FieldHandlingException | StorageException | SQLException | NoSuchFieldException ex) {
+                } catch (IllegalArgumentException | IllegalAccessException | IOException | InstantiationException | InvocationTargetException | NoSuchMethodException | FieldHandlingException | StorageException | SQLException | NoSuchFieldException | StorageCreationException | StorageConfInitializationException ex) {
                     throw new RuntimeException(ex);
                 }
             }
